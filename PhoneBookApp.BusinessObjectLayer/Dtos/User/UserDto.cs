5705950cd902e47edc98772ac5d2086a131bd791
anju0010 <r.anju@aspiromtechnologies.com>
2024-05-31 23:51:08 +0530

    UI

---
+++ PhoneBookApp.BusinessObjectLayer/Dtos/User/UserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookApp.BusinessObjectLayer.Dtos
+{
+    public class UserDto
+    {
+        public long Id { get; set; }
+        public string RegistrationId { get; set; }
+        public string Password { get; set; }
+        public string RecoveryEmail { get; set; }
+        public string OneTimePassword { get; set; }
+    }
+}
---
+++ PhoneBookApp.BusinessObjectLayer/Dtos/User/UserRegistrationDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookApp.BusinessObjectLayer.Dtos
+{
+    public class UserRegistrationDto
+    {
+        public string RegistrationId { get; set; }
+        public string Password { get; set; }
+        public string RecoveryEmail { get; set; }
+    }
+}
